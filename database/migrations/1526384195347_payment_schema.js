a33ab569d34530ad920fca2a8408f0c3b308eb7c
Mazino Ukah <ewomaukah@yahoo.com>
2018-05-15 12:46:11 +0100

    Added migration file and model for payment

---
+++ database/migrations/1526384195347_payment_schema.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class PaymentSchema extends Schema {
+  up () {
+    this.create('payments', (table) => {
+      table.increments()
+      table
+        .integer('store_id', 255)
+        .notNullable()
+        .references('id')
+        .inTable('stores')
+        .onDelete('cascade')
+        .onUpdate('cascade')
+      table.string('license_key').nullable()
+      table.boolean('paid').defaultTo(false)
+      table.date('trial_ends_at')
+      table.date('trial_starts_at')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('payments')
+  }
+}
+
+module.exports = PaymentSchema
