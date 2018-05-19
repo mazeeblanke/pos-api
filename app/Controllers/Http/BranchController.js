c7a5137dfd234012384367631e8f02c1364f092e
i5dr0id <i5dr0id308@gmail.com>
2018-05-19 14:07:21 +0100

    add branchcontroller

---
+++ app/Controllers/Http/BranchController.js
@@ -0,0 +1,33 @@
+'use strict'
+
+class BranchController {
+}
+
+module.exports = BranchController
+'use strict'
+
+
+class BranchController {
+  async index () {
+  }
+
+  async create () {
+  }
+
+  async store () {
+  }
+
+  async show () {
+  }
+
+  async edit () {
+  }
+
+  async update () {
+  }
+
+  async destroy () {
+  }
+}
+
+module.exports = BranchController
