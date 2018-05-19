5845c380ad9b6d703ebccb7612e0b1cd7abf7947
Mazino Ukah <ewomaukah@yahoo.com>
2018-05-19 12:08:26 +0100

    Added getter for currency to branch model

--- app/Models/Branch.js
+++ app/Models/Branch.js
@@ -3,6 +3,11 @@
 const Model = use('Model')
 
 class Branch extends Model {
+
+  getCurrency (currency) {
+    return JSON.parse(currency)
+  }
+
   store() {
     return this.belongsTo('App/Models/Store', 'store_id')
   }
